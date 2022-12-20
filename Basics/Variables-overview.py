64516e0d4b05787d9191d29ff76bceb845a5f2f3
BlckR0SE <121127705+BlckR0SE@users.noreply.github.com>
2022-12-20 22:57:35 +0330

    Create Variables-overview.py

---
+++ Basics/Variables-overview.py
@@ -0,0 +1,42 @@
+
+# In Python, a variable is a storage location that is used to hold a value or reference to an object. 
+# Variables are used to store data in a program, and they can be modified and used later on.
+# Here is an example of how to define a variable in Python:
+# --------------------------
+
+message = "Hello, world!"
+
+# --------------------------
+# In this example, the variable message is being assigned the string value "Hello, world!".
+# The variable message can then be used to refer to this value later on in the program
+# Here is an example of how to use a variable:
+# --------------------------
+
+message = "Hello, world!"
+print(message)
+
+# --------------------------
+# This code will output the string "Hello, world!" to the console.
+# It is important to note that variables in Python do not have a fixed type, meaning that a variable can be assigned a value of any type,
+# and the type of the variable can change at any time. For example, the following code is valid:
+# --------------------------
+
+message = "Hello, world!"
+print(message)
+
+message = 42
+print(message)
+
+message = [1, 2, 3]
+print(message)
+
+# --------------------------
+# This code will output the following to the console:
+# --------------------------
+
+# Hello, world!
+# 42
+# [1, 2, 3]
+
+
+
