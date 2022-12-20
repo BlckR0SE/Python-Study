211eecfc2600841ad7729c3093627508dc0edf7f
BlckR0SE <121127705+BlckR0SE@users.noreply.github.com>
2022-12-20 22:45:10 +0330

    Update Hello-Wolrd.py

--- Basics/Hello-Wolrd.py
+++ Basics/Hello-Wolrd.py
@@ -1,1 +1,6 @@
-# hello world learning
+print("Hello, world!")
+
+#This line of code will print the string "Hello, world!" to the console.
+#In python, the 'print' function is used to output text to the console. The text that you want to print is called a string
+#and it is written by enclosing it in quotation marks. In this case, we are printing the string "Hello, World!"
+# which is a common way to test that a program is working correctly
